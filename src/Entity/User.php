b9f925ae6361d303c4f55255b8759f7a6944c210
Sebastien Mora <mora.seb.c@gmail.com>
2023-08-03 16:55:25 +0200

    47

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -166,4 +166,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->prenom . " " . $this->nom;
+    }
 }
