cdf41a1b4c6cb449e307784a4804de1f6025710a
Sebastien Mora <mora.seb.c@gmail.com>
2023-08-04 14:01:32 +0200

    57

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Commentaires;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommentairesRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin')]
 class AdminController extends AbstractController
@@ -15,5 +20,29 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    #[Route('/validCom/{id}', name: 'valid_com_admin')]
+    public function validCom(CommentairesRepository $commentairesRepository, EntityManagerInterface $entityManager, $id): Response
+    {
+        $foundCommentaires = $commentairesRepository->findBy(['id' => $id]);
+        $commentaire = $foundCommentaires[0];
+        $commentaire->setStatus(1);
+        $entityManager->persist($commentaire);
+        $entityManager->flush();
+        return $this->render('admin/commentaires/index.html.twig', [
+            'commentaires' => $commentairesRepository->findBy(['status' => 0]),
+        ]);
+    }
+
+    #[Route('/deleteCom/{id}', name: 'delete_com_admin', methods: ['POST'])]
+    public function deleteCom(Request $request, Commentaires $commentaire, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $commentaire->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($commentaire);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_account_commentaires', ['id' => $commentaire->getAuteur()->getId()], Response::HTTP_SEE_OTHER);
     }
 }
